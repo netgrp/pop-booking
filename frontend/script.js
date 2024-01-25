a2ecc9ef928b38bd14d81734e88802fb00537fd8
Thomas Conrad <thomas.conrad@tmrw.com>
2024-01-25 13:29:36 +0100

    Add Fullcalendar sources

--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,46 @@
 function sendPostRequest(url, data) {
-    return fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 function updateForm() {
-    const form = document.getElementById('form')
-    const data = {
-        name: form.name.value,
-        email: form.email.value,
-        message: form.message.value,
-    }
-    console.log(data)
-    sendPostRequest('/new_booking', data)
-        .then((response) => {
-            if (response.status === 200) {
-                alert('Message sent!')
-            } else {
-                alert('Something went wrong...')
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error)
-        })
+  const form = document.getElementById("form");
+  const data = {
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value,
+  };
+  console.log(data);
+  sendPostRequest("/new_booking", data)
+    .then((response) => {
+      if (response.status === 200) {
+        alert("Message sent!");
+      } else {
+        alert("Something went wrong...");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function switchPages(toPage) {
+  let messageDiv = document.getElementById("notices-div");
+  let calendarDiv = document.getElementById("calendar-div");
+
+  switch (toPage) {
+    case "notices":
+      messageDiv.style.display = "block";
+      calendarDiv.style.display = "none";
+      break;
+    case "calendar":
+      messageDiv.style.display = "none";
+      calendarDiv.style.display = "block";
+      break;
+  }
 }
